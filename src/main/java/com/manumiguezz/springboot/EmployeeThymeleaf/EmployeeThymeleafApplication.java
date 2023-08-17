7f8e00c7d138d52f4dcc6af9bd76cc97f8930cd0
Manuel Miguez Lauría <manuelmiguezlauria@gmail.com>
2023-08-17 20:47:01 -0300

    controller package added to java main, spring mvc controller added

---
+++ src/main/java/com/manumiguezz/springboot/EmployeeThymeleaf/EmployeeThymeleafApplication.java
@@ -0,0 +1,13 @@
+package com.manumiguezz.springboot.EmployeeThymeleaf;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EmployeeThymeleafApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EmployeeThymeleafApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/manumiguezz/springboot/EmployeeThymeleaf/EmployeeThymeleafApplicationTests.java
@@ -0,0 +1,13 @@
+package com.manumiguezz.springboot.EmployeeThymeleaf;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EmployeeThymeleafApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
