6544e57e7f6cf95dadcde6e1623706a639cd1a46
Manuel Miguez <manuelmiguezlauria@gmail.com>
2023-09-06 23:03:25 -0300

    customer class finished, and controller updated with model

--- src/main/java/com/manumiguezz/springboot/EmployeeThymeleaf/Customer.java
+++ src/main/java/com/manumiguezz/springboot/EmployeeThymeleaf/Customer.java
@@ -1,9 +1,15 @@
 package com.manumiguezz.springboot.EmployeeThymeleaf;
 
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Size;
+
 public class Customer {
 
     private String firstName;
-    private String lastName;
+
+    @NotNull(message = "is required")
+    @Size(min = 1, message = "is required")
+    private String lastName = "";
 
     public String getFirstName() {
         return firstName;
--- src/main/java/com/manumiguezz/springboot/EmployeeThymeleaf/controller/SpringMVCController.java
+++ src/main/java/com/manumiguezz/springboot/EmployeeThymeleaf/controller/SpringMVCController.java
@@ -1,5 +1,6 @@
 package com.manumiguezz.springboot.EmployeeThymeleaf.controller;
 
+import com.manumiguezz.springboot.EmployeeThymeleaf.Customer;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -11,5 +12,11 @@ public class SpringMVCController {
     public String htmlModel(Model theModel) {
         theModel.addAttribute("theDate", new java.util.Date());
         return "htmlModel";
+    }
+
+    @GetMapping("/")
+    public String showForm (Model theModel) {
+        theModel.addAttribute("customer", new Customer());
+        return "customer-form";
     }
 }
