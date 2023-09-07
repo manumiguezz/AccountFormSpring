53d01b56468eece8f1afa80063e52d55547dd480
Manuel Miguez <manuelmiguezlauria@gmail.com>
2023-09-06 23:15:32 -0300

    controller updated

--- src/main/java/com/manumiguezz/springboot/EmployeeThymeleaf/controller/SpringMVCController.java
+++ src/main/java/com/manumiguezz/springboot/EmployeeThymeleaf/controller/SpringMVCController.java
@@ -1,9 +1,13 @@
 package com.manumiguezz.springboot.EmployeeThymeleaf.controller;
 
 import com.manumiguezz.springboot.EmployeeThymeleaf.Customer;
+import jakarta.validation.Valid;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
 
 @Controller
 public class SpringMVCController {
@@ -18,5 +22,18 @@ public class SpringMVCController {
     public String showForm (Model theModel) {
         theModel.addAttribute("customer", new Customer());
         return "customer-form";
+    }
+
+    @PostMapping("/processForm")
+    public String processForm(
+            @Valid @ModelAttribute("customer") Customer theCustomer,
+            BindingResult theBindingResult) {
+
+        if (theBindingResult.hasErrors()) {
+            return "customer-form";
+        } else {
+            return "customer-confirmation";
+        }
+
     }
 }
